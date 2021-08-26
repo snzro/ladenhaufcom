79f868df1d81c3d2d44ec328534341d4016d1147
Wilfried Ladenhauf <wladenhauf@gmail.com>
2021-08-26 07:13:34 +0200

    init

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function HomePage() {
+    return <body><div>ladenhauf.com</div><h1>wilfried ladenhauf</h1><a href="https://twitter.com/snzro/">https://twitter.com/snzro/</a></body>
+  }
+  
+  export default HomePage
