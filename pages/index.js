e357fa60f667ea64b9ef3796af4393e924b8c06a
Wilfried Ladenhauf <wladenhauf@gmail.com>
2024-10-24 05:13:18 +0200

    Delete pages directory

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-function HomePage() {
-    return <body><div>ladenhauf.com</div><h1>wilfried ladenhauf</h1><a href="https://twitter.com/snzro/">https://twitter.com/snzro/</a></body>
-  }
-  
-  export default HomePage
